f4a188c07da983375eeffeb68178c3bfe3473aa0
ooferi <pilipino115@gmail.com>
2024-04-05 09:49:13 -0500

    mewejajd;'sd'

--- GUI/GUI/GUI.py
+++ GUI/GUI/GUI.py
@@ -10,6 +10,31 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(rx.State):
     """The app state."""
+    
+class ExampleState(rx.State):
+    # A base var for the list of colors to cycle through.
+    colors: list[str] = [
+        "black",
+        "red",
+        "green",
+        "blue",
+        "purple",
+    ]
+
+    # A base var for the index of the current color.
+    index: int = 0
+
+    def next_color(self):
+        """An event handler to go to the next color."""
+        # Event handlers can modify the base vars.
+        # Here we reference the base vars `colors` and `index`.
+        self.index = (self.index + 1) % len(self.colors)
+
+    @rx.var
+    def color(self) -> str:
+        """A computed var that returns the current color."""
+        # Computed vars update automatically when the state changes.
+        return self.colors[self.index]
 
 
 def index() -> rx.Component:
@@ -17,15 +42,17 @@ def index() -> rx.Component:
         #rx.theme_panel(),
         rx.grid(
             rx.foreach(
-            rx.Var.range(16),
-            lambda i: rx.button(f"{i + 1}", height="8.5vh", color_scheme="gray", on_click=)
+            rx.Var.range(25),
+            lambda i: rx.button(height="18vh", width = "18vh" )
             ),
-        columns="4",  
+        
+        columns="5",  
         spacing="1",
-        width="60%",
+        width="50%",
     ),
 
         height="100vh",
+        width = "185vh"
     )
 
 
