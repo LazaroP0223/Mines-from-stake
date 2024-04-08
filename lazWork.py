d2f92a2ca889da745e26e4fb8bfb349ddf8aff10
Lazaro Perez <lperez3568@stu.neisd.net>
2024-04-08 09:58:03 -0500

    arrrays

--- lazWork.py
+++ lazWork.py
@@ -1,17 +1,19 @@
 import random
 arr = [[0,0,0,0,0], [0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 gemCount = 0
-gemMax = 4
+gemMax = 24
 myList = [1, 2, 3, 4, 0]
 
 print(arr)
 def gemRandomization(array, gCount, gMax):
 
     while gCount < gMax:
-        gCount = gCount + 1
         x = random.choice(myList)
         y = random.choice(myList)
-        array[x][y] = 1
+        
+        if(array[x][y] != 1):
+            gCount = gCount + 1
+            array[x][y] = 1
                     
 
 gemRandomization(arr,gemCount, gemMax)
